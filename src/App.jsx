14f2b390301202f3e08b3d71211764e924b980b9
Rahma Nader <r.nader@egac.rak.ae>
2024-11-04 11:28:24 +0200

    we're in the clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Header } from "./components/Header"
-import { Tabs } from "./components/Tabs"
-import { TodoInput } from "./components/TodoInput"
-import { TodoList } from "./components/TodoList"
+import { Header } from "./components/Header";
+import { Tabs } from "./components/Tabs";
+import { TodoInput } from "./components/TodoInput";
+import { TodoList } from "./components/TodoList";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
   // const todos = [
@@ -14,28 +14,27 @@ function App() {
   // ]
 
   const [todos, setTodos] = useState([
-    { input: 'Hello! Add your firs todo!', complete: false},
-  ])
-  const [selectedTab, setSelectedTab] = useState('Open')
+    { input: "Hello! Add your firs todo!", complete: false },
+  ]);
+  const [selectedTab, setSelectedTab] = useState("All");
 
   function handleAddTodo(newTodo) {
-    //we add to the existing list but we create a new one a dublicate with the extra new todo at the end of the list 
-    const newTodoList = [...todos, { input: newTodo, complete: false }]
+    //we add to the existing list but we create a new one a dublicate with the extra new todo at the end of the list
+    const newTodoList = [...todos, { input: newTodo, complete: false }];
     //now we use the hook to set the new to do to replace the old to do list
-    setTodos(newTodoList)
-    handleSaveData(newTodoList)
+    setTodos(newTodoList);
+    handleSaveData(newTodoList);
   }
 
   function handleCompleteTodo(index) {
     // update/edit/modify
-    let newTodoList = [...todos]
-    let completedTodo = todos[index]
-    completedTodo['complete'] = true
-    newTodoList[index] = completedTodo
-    setTodos(newTodoList)
-    handleSaveData(newTodoList)
+    let newTodoList = [...todos];
+    let completedTodo = todos[index];
+    completedTodo["complete"] = true;
+    newTodoList[index] = completedTodo;
+    setTodos(newTodoList);
+    handleSaveData(newTodoList);
   }
-  
 
   // function handleEditTodo(index) {
   //   // step 1 - create a duplicate array
@@ -48,30 +47,41 @@ function App() {
 
   function handleDeleteTodo(index) {
     let newTodoList = todos.filter((val, valIndex) => {
-      return valIndex !== index
-    })
-    setTodos(newTodoList)
-    handleSaveData(newTodoList)
+      return valIndex !== index;
+    });
+    setTodos(newTodoList);
+    handleSaveData(newTodoList);
   }
 
   function handleSaveData(currTodos) {
-    localStorage.setItem('todo-app', JSON.stringify({ todos: currTodos }))
+    localStorage.setItem("todo-app", JSON.stringify({ todos: currTodos }));
   }
 
   useEffect(() => {
-    if (!localStorage || !localStorage.getItem('todo-app')) { return }
-    let db = JSON.parse(localStorage.getItem('todo-app'))
-    setTodos(db.todos)
-  }, [])
+    if (!localStorage || !localStorage.getItem("todo-app")) {
+      return;
+    }
+    let db = JSON.parse(localStorage.getItem("todo-app"));
+    setTodos(db.todos);
+  }, []);
 
   return (
     <>
       <Header todos={todos} />
-      <Tabs selectedTab={selectedTab} setSelectedTab={setSelectedTab} todos={todos} />
-      <TodoList handleCompleteTodo={handleCompleteTodo} handleDeleteTodo={handleDeleteTodo} selectedTab={selectedTab} todos={todos} />
+      <Tabs
+        selectedTab={selectedTab}
+        setSelectedTab={setSelectedTab}
+        todos={todos}
+      />
+      <TodoList
+        handleCompleteTodo={handleCompleteTodo}
+        handleDeleteTodo={handleDeleteTodo}
+        selectedTab={selectedTab}
+        todos={todos}
+      />
       <TodoInput handleAddTodo={handleAddTodo} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
